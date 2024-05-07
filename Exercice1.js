f0cb00f54612df8f606533d5ecafe6fdf90a7867
SylpheLaCodeuse <160395235+SylpheLaCodeuse@users.noreply.github.com>
2024-05-07 09:12:08 +0200

    Add files via upload

---
+++ Exercice1.js
@@ -0,0 +1,28 @@
+const num1 = 2;
+const num2 = "1";
+
+function testNum(num1, num2) {
+    let result;
+    if (typeof num1 !== 'number' || typeof num2 !== 'number'); 
+
+    {result = 'null';} 
+    
+    else
+
+    {result = num1 + num2;}
+    
+    return result;
+}
+
+console.log(testNum(num1, num2));
+
+
+
+
+
+
+
+  
+
+  
+ 
