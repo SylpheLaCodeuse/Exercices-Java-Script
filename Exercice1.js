29ae058898cbd2d39f7238a43c755c6835b4cccf
SylpheLaCodeuse <160395235+SylpheLaCodeuse@users.noreply.github.com>
2024-05-08 17:13:16 +0200

    Add files via upload

--- Exercice1.js
+++ Exercice1.js
@@ -1,20 +1,24 @@
-const num1 = 2;
-const num2 = "1";
+const num1 = 1;
+const num2 = "2";
+//mes deux paramètres
 
 function testNum(num1, num2) {
+//ma fonction
+
     let result;
-    if (typeof num1 !== 'number' || typeof num2 !== 'number'); 
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') 
 
     {result = 'null';} 
     
     else
 
-    {result = num1 + num2;}
+    {result = parseInt(num1) + parseInt(num2);}
     
     return result;
 }
 
-console.log(testNum(num1, num2));
+console.log(testNum(num1, num2)); 
+
 
 
 
---
+++ Exercice3.js
@@ -0,0 +1,23 @@
+const tableau = [2, 4, "jambon", 8, null, 10]
+//mon tableau
+
+function AddPairNumbers(tableau)
+    {let somme = 0;
+        for(let numbers of tableau)
+
+        {if (numbers % 2 === 0)
+    {somme += numbers;} 
+        }
+//ma fonction qui n'additionne que les nombres pairs
+
+if (Array.isArray(tableau));
+//vérifie que le tableau est bien un tableau
+
+if (typeof tableau !== 'number');
+//vérifie que les valeurs sont des nombres
+
+    return somme;
+//renvoie la somme
+    }
+
+console.log(AddPairNumbers(tableau));
